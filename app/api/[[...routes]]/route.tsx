de5f91a6a19426544fcc40f0dafff49c6e249997
Kevin O'Connell <kevinoconnell42@gmail.com>
2024-03-13 23:01:10 -0400

    Update route.tsx

--- app/api/[[...routes]]/route.tsx
+++ app/api/[[...routes]]/route.tsx
@@ -7,25 +7,60 @@ import axios from "axios";
 const app = new Frog({
   basePath: "/api",
   //comment out below for local
-  // hub: neynar({ apiKey: process.env.NEYNAR_API_KEY as string }),
+  hub: neynar({ apiKey: process.env.NEYNAR_API_KEY as string }),
+  initialState: {
+    prompt: "",
+  },
 });
 
-app.frame("/", async (c) => {
-  const { buttonValue, status, inputText } = c;
-  console.log(c);
-  //Check process of TX
-  const isFinished = false;
-  const res = await axios.post(
-    "https://79828676f8aea1eb.agent.propel.autonolas.tech/generate_farcaster",
-    {
-      ...c,
-    },
-    {
-      headers: {
-        Authorization: `Bearer ${process.env.NEXT_PUBLIC_BEARER_TOKEN}`,
-      },
+app.frame("/frame", async (c) => {
+  const { buttonValue, status, inputText, frameData, deriveState } = c;
+  const state = deriveState((previousState) => {
+    if (buttonValue === "generate") {
+      return { ...previousState, prompt: inputText };
     }
-  );
+  });
+  const { fid } = frameData || {};
+
+  const isFinished = false;
+  //Check process of TX
+  let text = inputText;
+  if (!fid && state.prompt) {
+    text = "There was an error generating your video, please try again!";
+  } else if (fid && state.prompt) {
+    const users = await axios.get(
+      `https://api.neynar.com/v2/farcaster/user/bulk?fids=${fid}`,
+      {
+        headers: {
+          accept: "application/json",
+          api_key: process.env.NEYNAR_API_KEY,
+        },
+      }
+    );
+
+    if (!users.data || !users.data.users || !users.data.users.length) {
+      text = "There was an error generating your video, please try again!";
+    }
+
+    const addresses = users.data.users[0].verified_addresses;
+    let address =
+      addresses.eth_addresses && addresses.eth_addresses.length
+        ? addresses.eth_addresses[0]
+        : users.data.users[0].custody_address;
+
+    // const res = await axios.post(
+    //   "https://79828676f8aea1eb.agent.propel.autonolas.tech/generate_farcaster",
+    //   {
+    //     ...c,
+    //     address,
+    //   },
+    //   {
+    //     headers: {
+    //       Authorization: `Bearer ${process.env.NEXT_PUBLIC_BEARER_TOKEN}`,
+    //     },
+    //   }
+    // );
+  }
 
   //TODO: check if the video is ready, if so link, otherwise refresh
   return c.res({
@@ -42,7 +77,7 @@ app.frame("/", async (c) => {
           textAlign: "center", // This centers the text if it breaks into multiple lines
         }}
       >
-        {inputText ? inputText : "Enter a prompt to generate a video!"}
+        {text ? text : "Enter a prompt to generate a video!"}
       </div>
     ),
     intents: [
